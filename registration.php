d5df4afa597e581d86a99db174c9eecd342e8e27
KalsangYangtso <Kalsang.Yangtso92@guttmanmail.cuny.edu>
2016-07-27 19:07:51 -0400

    changes

--- registration.php
+++ registration.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         
-      <form action="raction_page.php" method="post"> 
+      <form action="action_page.php" method="post"> 
            <fieldset>
     <legend>Sign Up</legend>
    First name:<br>
