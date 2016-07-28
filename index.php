d0332f1b1674674851e909de1b147ecd4c29cb3b
KalsangYangtso <Kalsang.Yangtso92@guttmanmail.cuny.edu>
2016-07-28 15:21:45 -0400

    changes

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h2>Welcome to Bookazon</h2>
+        <a href="index.php">Home</a>
+        <a href="login.php">Login</a>
+        <a href="registration.php">Sign Up</a>
+        <a href="cart.php">Cart</a>
+        <a href="shop.php">Shop</a>
+         <a href="sale.php">Sale</a>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
