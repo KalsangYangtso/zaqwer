cda8a6876c62570ea6f3e74b8d919a150429df52
KalsangYangtso <Kalsang.Yangtso92@guttmanmail.cuny.edu>
2016-07-30 12:53:31 -0400

    changes

--- action_page.php
+++ action_page.php
@@ -13,5 +13,99 @@ and open the template in the editor.
      
         <h4>Registration confirmed</h4> 
        
+<?php
+include ('script.php');
+if (isset($_POST['formsubmitted'])) {
+    $error = array(); 
+    if (empty($_POST['username'])) { 
+        $error[] = 'Please Enter a name '; 
+    } else {
+        $username = $_POST['username']; 
+    }
+
+    if (empty($_POST['e-mail'])) {
+        $error[] = 'Please Enter your Email ';
+    } else {
+
+        if (filter_var($_POST['e-mail'], FILTER_VALIDATE_EMAIL)) {
+            //for email validation (refer: http://us.php.net/manual/en/function.filter-var.php)
+
+            $email = $_POST['e-mail'];
+        } else {
+            $error[] = 'Your EMail Address is invalid  ';
+        }
+
+    }
+
+    if (empty($_POST['password'])) {
+        $error[] = 'Please Enter Your Password ';
+    } else {
+        $password = $_POST['password'];
+    }
+
+    if (empty($error))
+
+    { // If everything's OK...
+
+
+        $query = "SELECT * FROM members  WHERE username ='$username'";
+        $result = mysqli_query($dbc, $query); // here $dbc is your mysqli $link
+        if (!$result) {
+            echo ' Database Error Occured ';
+        }
+
+        if (mysqli_num_rows($result) == 0) { // IF no previous user is using this username.
+
+            $query = "INSERT INTO `members` ( `username`, `email`, `password`) VALUES ( '$name', '$email', '$password')";
+
+            $result = mysqli_query($dbc, $query);
+            if (!$result) {
+                echo 'Query Failed ';
+            }
+
+            if (mysqli_affected_rows($dbc) == 1) { //If the Insert Query was successfull.
+
+                // Send an email
+
+                // Finish the page:
+                echo '<div class="success">Thank you for registering! A confirmation email has been sent to ' . $email . ' Please click on the Activation Link to Activate your account </div>';
+
+            } else { // If it did not run OK.
+                echo '<div class="errormsgbox">You could not be registered due to a system error. We apologize for any inconvenience.</div>';
+            }
+
+        } else { // The username is not available.
+            echo '<div class="errormsgbox" >That username has already been registered.
+</div>';
+        }
+
+    } else { //If the "error" array contains error msg , display them.... e.g....
+
+        echo '<div class="errormsgbox"> <ul>';
+        foreach ($error as $key => $values) {
+
+            echo '  <li>' . $values . '</li>';
+
+        }
+        echo '</ul></div>';
+
+    }
+
+    mysqli_close($dbc); //Close the DB Connection
+
+} // End of the main Submit conditional.
+
+?>
+        <?php
+        $sql=mysql_query("SELECT FROM users (username, password, email) WHERE username=$fusername");
+ if(mysql_num_rows($sql)>=1)
+   {
+    echo"name already exists";
+   }
+ else
+    {
+   //insert query goes here
+    }
+    ?>
     </body>
 </html>
--- script.sql
+++ script.sql
@@ -1,1 +1,91 @@
 
+drop database  if exists bookazon;
+create database bookazon;
+
+use  bookazon ;
+
+
+CREATE TABLE CUSTOMER
+(
+  CID INT NOT NULL,
+  CFName varchar(25) NOT NULL,
+  CLName varchar(25) NOT NULL,
+  CUserName varchar(25) NOT NULL,
+  CPassword varchar(25) NOT NULL,
+  CBBuildingNo char(25) NOT NULL,
+CBAptNo char(15) NOT NULL,
+CBStreet/Avenue varchar(25) NOT NULL,
+CBCity varchar(25) NOT NULL,
+CBState varchar(25) NOT NULL,
+CBZipcode numeric (15) NOT NULL,
+CSBuildingNo char(25) NOT NULL,
+CSAptNo char(15) NOT NULL,
+CSStreet/Avenue varchar(25) NOT NULL,
+CSCity varchar(25) NOT NULL,
+CSState varchar(25) NOT NULL,
+CSZipcode numeric (15) NOT NULL,
+  
+  PRIMARY KEY (CID),
+  UNIQUE (CUserName)
+);
+
+CREATE TABLE PRODUCT
+(
+  PID INT NOT NULL,
+  PName  varchar(25) NOT NULL,
+  PPrice INT NOT NULL,
+  Quantity INT NOT NULL,
+  PRIMARY KEY (PID)
+);
+
+CREATE TABLE TRANSACTIONS
+(
+  TID  varchar(10) NOT NULL,
+  TDATE date NOT NULL,
+  TTotal INT NOT NULL,
+  PRIMARY KEY (TID)
+);
+
+CREATE TABLE CART
+(
+  CID INT NOT NULL,
+  PRIMARY KEY (CID),
+  FOREIGN KEY (CID) REFERENCES CUSTOMER(CID)
+);
+
+CREATE TABLE INCLUDES
+(
+  NoOfitems INT ,
+  SoldPrice INT ,
+  PID INT NOT NULL,
+  TID varchar(10) NOT NULL,
+  PRIMARY KEY (PID, TID),
+  FOREIGN KEY (PID) REFERENCES PRODUCT(PID),
+  FOREIGN KEY (TID) REFERENCES TRANSACTIONS(TID)
+);
+
+CREATE TABLE PLACES
+(
+  TID varchar(10) ,
+  CID INT,
+  PRIMARY KEY (TID, CID),
+  FOREIGN KEY (TID) REFERENCES TRANSACTIONS(TID),
+  FOREIGN KEY (CID) REFERENCES CUSTOMER(CID)
+);
+
+CREATE TABLE CUSTOMER_CCreditcards
+(
+  CCreditcards  varchar(25) NOT NULL,
+  CID INT NOT NULL,
+  PRIMARY KEY (CCreditcards, CID),
+  FOREIGN KEY (CID) REFERENCES CUSTOMER(CID)
+);
+
+CREATE TABLE isAdded
+(
+  PID INT NOT NULL,
+  CID INT NOT NULL,
+  FOREIGN KEY (PID) REFERENCES PRODUCT(PID),
+  FOREIGN KEY (CID) REFERENCES CART(CID)
+);
+
