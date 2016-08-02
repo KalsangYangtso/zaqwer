1b641100d1503e8dd428149b74603d1ceb601e1c
KalsangYangtso <Kalsang.Yangtso92@guttmanmail.cuny.edu>
2016-08-02 15:17:34 -0400

    changes

--- index.php
+++ index.php
@@ -11,31 +11,25 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-       
-    $link = mysqli_connect('phpmyadmin','kay','dtycts16');
-  
-            if(!$link)
-            {
-                $output='Unable to connect to the Database Server.';
-                 include'output.html.php'; 
-                     exit();
-            }
-    if(!mysqli_set_charset($link, 'utf8'))
-    {
-        $output='Unable to set database connection encoding.';
-        include'output.html.php'; 
-        exit();
-        
-    }
-    if(!mysqli_select_db($link, 'kay'))
-    {
-        $output='Unable to connect to the Database Server.';
-        include'output.html.php'; 
-        exit();
-        
-    }
-  $output='Database connection established.';
-        include'output.html.php'; 
-         ?>
+        $link = mysqli_connect('localhost', 'kay', 'dtycts16');
+
+        if (!$link) {
+            $output = 'Unable to connect to the Database Server.';
+            include'output.html.php';
+            exit();
+        }
+        if (!mysqli_set_charset($link, 'utf8')) {
+            $output = 'Unable to set database connection encoding.';
+            include'output.html.php';
+            exit();
+        }
+        if (!mysqli_select_db($link, 'kay')) {
+            $output = 'Unable to connect to the Database Server.';
+            include'output.html.php';
+            exit();
+        }
+        $output = 'Database connection established.';
+        include'output.html.php';
+        ?>
     </body>
 </html>
